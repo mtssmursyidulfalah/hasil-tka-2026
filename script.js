cfc3f49dc322420b1a3f7d0f158df905d0517785
mtssmursyidulfalah <hafidz.mhmd04@gmail.com>
2026-05-27 16:04:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,74 @@
-const siswa = [
+function cekData(){
 
-  {
-    nisn:"1234567890",
-    nama:"Ahmad Fauzi",
-    kelas:"IX A",
-    peserta:"25-001",
+  const nisn =
+  document.getElementById(
+    "nisn"
+  ).value.trim();
 
-    matematika:"88",
-    kriteria_mtk:"Sangat Baik",
+  const data = siswa.find(
+    s => s.nisn === nisn
+  );
 
-    indonesia:"82",
-    kriteria_indo:"Baik"
+  if(!data){
+
+    alert(
+      "Data tidak ditemukan"
+    );
+
+    return;
   }
 
-];
+  document.body.innerHTML = `
+
+  <div class="glow"></div>
+  <div class="glow"></div>
+
+  <div class="card">
+
+    <img
+    src="logo.png"
+    class="logo">
+
+    <h1>
+      HASIL TKA
+    </h1>
+
+    <div class="hasil">
+
+      <p>
+      <b>Nama:</b><br>
+      ${data.nama}
+      </p>
+
+      <p>
+      <b>Kelas:</b><br>
+      ${data.kelas}
+      </p>
+
+      <p>
+      <b>No Peserta:</b><br>
+      ${data.peserta}
+      </p>
+
+      <hr>
+
+      <p>
+      <b>Matematika</b><br>
+      Nilai: ${data.matematika}<br>
+      Kriteria:
+      ${data.kriteria_mtk}
+      </p>
+
+      <p>
+      <b>Bahasa Indonesia</b><br>
+      Nilai: ${data.indonesia}<br>
+      Kriteria:
+      ${data.kriteria_indo}
+      </p>
+
+    </div>
+
+  </div>
+
+  `;
+}
