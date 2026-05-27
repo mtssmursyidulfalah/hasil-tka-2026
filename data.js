389f4023babdf506aa35358d3d427d2e4f389e42
mtssmursyidulfalah <hafidz.mhmd04@gmail.com>
2026-05-27 16:03:53 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,17 @@
 
+const siswa = [
+
+  {
+    nisn:"1234567890",
+    nama:"Ahmad Fauzi",
+    kelas:"IX A",
+    peserta:"25-001",
+
+    matematika:"88",
+    kriteria_mtk:"Sangat Baik",
+
+    indonesia:"82",
+    kriteria_indo:"Baik"
+  }
+
+];
